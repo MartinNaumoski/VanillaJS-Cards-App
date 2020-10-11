9bafc5af1874bdeeb98683aa60e3b99d39a8f93c
Martin96oh <martin96naumoski@hotmail.com>
2020-10-11 22:52:50 +0200

    added filters

--- task2/cards.js
+++ task2/cards.js
@@ -1,3 +1,4 @@
+ 
 
 function takeCardsData() {
     const api_url = "https://api.magicthegathering.io/v1/cards?random=true&pageSize=100&language=English";
@@ -14,13 +15,34 @@ function takeCardsData() {
     getapi(api_url).then(data => {
         showCards(data.cards);
     });
+    takeDataForTypes();
+}
+function takeDataForTypes(){
+    const api_url = "https://api.magicthegathering.io/v1/types";
+    async function getTypes(url) {
+        const response = await fetch(url);
+        let data = await response.json();
+        return data;
+    }
+    getTypes(api_url).then(data => {
+       console.log(data);
+       let type = [];
+       var filter = document.getElementById("filters");
+       var select = document.getElementById("types");
+       data.types.forEach(element => {
+        console.log(element);
+        var option = document.createElement("option");
+        option.text = element;
+        select.appendChild(option);
+       });
+       filter.appendChild(select);
+    });
 }
 function takeHeaderData(name){
     let header = document.getElementById("header");
     header.innerHTML += name;
 }
 function showCards(data){
-    console.log(data);
     let allCards =  document.getElementById("allCards");
     data.forEach(element => {
         let newCard = document.createElement("li");
@@ -33,13 +55,12 @@ function showCards(data){
             colors += element + " ";
         });
         let imageUrl = element.imageUrl != undefined ? element.imageUrl : 'https://lh3.googleusercontent.com/proxy/bUpiCKp_F2jhritiWIr3X-5WG1Y9Hz15swRh9pa0WRjUdAq5HCoO9cz1EjoqYWYLtZJDMTpWNWNU4nAhTolev9_o30RHFgoLdCvihiw'; 
-        console.log(imageUrl);
         newCard.innerHTML =
         "<img class='left' src="+imageUrl+">"
-        +"<br>'<h2 class='right'>"+element.artist+"</h2>'<div class='right'>Name: " + element.name + "<br>"
+        +"<br><h2 class='right'>"+element.artist+"</h2><div class='right'>Name: " + element.name + "<br>"
         +"Types: "+ types +"</div>"
         +"<div class='right'>Name of the set: " + element.setName 
-        +"<br> Colors: "+ colors +"</div>";
+        +"<br><div> Colors: "+ colors +"</div></div>";
         allCards.appendChild(newCard);
     });
 }
@@ -68,7 +89,6 @@ function ascendingSort(data){
     sortAccending = sortAccending.sort();
     let allCards =  document.getElementById("allCards");
     allCards.innerHTML= '';
-    console.log(sortAccending);
     sortAccending.forEach(element => {
         let newCard = document.createElement("li");
         newCard.innerHTML = element;
@@ -76,7 +96,6 @@ function ascendingSort(data){
     });
 }
 function decendingSort(data){
-    console.log(data);
     let decendingSort = [];
     for(let i=0;i<data.length;i++){
         decendingSort[i] = data[i].innerHTML;
@@ -90,5 +109,25 @@ function decendingSort(data){
         allCards.appendChild(newCard);
     });
 }
+// function filterByColor(){
+//     let select = document.getElementById("filterColor");
+//     let selectedColor = select.options[select.selectedIndex].textContent;
+//     ul = document.getElementById("allCards");
+//     li = ul.getElementsByTagName("li");
+//     let allCards =  document.getElementById("allCards");
+//     for (i = 0; i < li.length; i++) {
+//         console.log(li[i]);
+//         let length = li[i].childNodes[4].childNodes[2].innerText.length;
+//         color = li[i].childNodes[4].childNodes[2].innerText.substring(7,length);
+//         if(color == selectedColor){
+//             li[i].style.display = '';
+//         }
+//         else{
+//             li[i].style.display = 'none';
+
+//         }
+//     }
+// }
+
 
 
